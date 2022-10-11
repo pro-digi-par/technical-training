7c4c23f10a88a6a7362081e57b8217cd32bb23ef
Josef Dietl <jdietl@pro-digi-par.de>
2022-10-11 12:08:34 +0200

    Fixing a bug in naming conventions

--- estate/models.py
+++ estate/models.py
@@ -38,11 +38,11 @@ class EstatePropertyTypeModel(models.Model):
     name = fields.Char(required = True)
     
 class EstatePropertyOffer(models.Model):
-    _name = "estate_property_offer"
+    _name = "estate.property.offer"
     _description = "Offer on an object"
     price = fields.Float()
     status = fields.Selection(string='Status',
                           selection=[('accepted','Accepted'),('refused','Refused')],
                            copy=False)
     partner_id = fields.Many2one("res.partner", required = True)
-    property_id = fields.Many2one("estate.property", required = True)
+    property_id = fields.Many2one("estate_property", required = True)
