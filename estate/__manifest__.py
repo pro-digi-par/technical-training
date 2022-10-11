367e0f3f9352758cdc6d4a79a115f983a3dcc6ef
Josef Dietl <jdietl@pro-digi-par.de>
2022-10-11 10:47:17 +0200

    Adding UIs to the estate type

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,6 +5,7 @@
     "depends": ["base"],  # dependencies
     "data": ['security/ir.model.access.csv',
              'views/estate_property_views.xml',
+             'views/estate_type_views.xml',
              'views/estate_menus.xml'
              
     ],
