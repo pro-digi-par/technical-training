8cf59c8edbd585a2260a7fa38899b472942eb4cf
Josef Dietl <jdietl@pro-digi-par.de>
2022-10-11 10:06:30 +0200

    Catching up with Geoffrey

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,10 +3,10 @@
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": [
-    	'security/ir.model.access.csv',
-    	'views/estate_menus.xml',
-    	'views/estate_property_views.xml'
+    "data": ['security/ir.model.access.csv',
+             'views/estate_menus.xml',
+             'views/estate_property_views.xml'
+             
     ],
     "installable": True,
     'license': 'LGPL-3',
--- estate/models.py
+++ estate/models.py
@@ -1,19 +1,35 @@
 from odoo import models, fields
 
-class EstatePropertyModel(models.Model):
-    _name = "estate.property.model"
-    _description = "(whatever)"
+
+"""import pdb
+pdb.set_trace()"""
+
+class TestModel(models.Model):
+    _name = "estate_property"
+    _description = "My Estate property description"
     
-    name = fields.Char('Name', required=True)
-    active = fields.Boolean('Active', default=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability Date')
-    expected_price = fields.Float('Expected Price', required=True)   
-    selling_price = fields.Float('Selling Price')     
-    bedrooms = fields.Integer('# of bedrooms')
-    living_area = fields.Integer('Living Area')
-    facades = fields.Integer('Facades')           
-    garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False,default=fields.Date.add(fields.Date.today(), months=3))
+    expected_price= fields.Float(required=True)
+    selling_price=fields.Float(readonly=True,copy=False)
+    bedrooms=fields.Integer(default=2)
+    living_area=fields.Integer()
+    facades=fields.Integer()
+    garage=fields.Boolean()
+    garden=fields.Boolean()
+    garden_area=fields.Integer()
+    garden_orientation=fields.Selection(string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Used to choose your orientation")
+    
+    Active=fields.Boolean(default=False)
+    State=fields.Selection(string='State',
+                          selection=[('new','New'),('Offer_Received','Offer_Received'),('Offer_Accepted','Offer_Accepted'),('sold','Sold'),('canceled','canceled')],
+                           required=True,
+                           copy=False,
+                           default='new')
+    
+    
+ 
